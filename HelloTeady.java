5cc953ada0d350a01062c65541400050e24e260c
Anurag Jambhulkar <jambhulkaranurag12@gmail.com>
2022-03-10 13:47:02 +0530

    Create HelloTeady.java

---
+++ HelloTeady.java
@@ -0,0 +1,16 @@
+
+/*This Program will not run properly in onLINE coMPILERS BUT IF yoUR use IN IDE, javaCOMPILER theN it WILL work Fine */
+
+
+class HelloTeddyScritp {
+    public static void main(String[] args) {
+
+
+               for(int a=0 ; a<=5; a++)// CHange the Value of A to to Print your FAV no.of Teddy  
+        {
+        System.out.println("\\ /");
+        System.out.println("(..)");
+        System.out.println("(\uE022)");
+        }
+    }
+}
